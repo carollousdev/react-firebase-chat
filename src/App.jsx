5b3c3601ccae011472094ace0be1be9e34027014
carollousdev <carollous.webdev@gmail.com>
2025-06-16 12:55:45 +0700

    React Search Bar Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Detail from "./components/detail/Detail";
+import Chat from "./components/chat/Chat";
+import List from "./components/list/List";
+
 const App = () => {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  );
 };
 
 export default App;
