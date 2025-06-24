9edcf96cc48746a2f2019859db5b72b008f470bd
carollousdev <carollous.webdev@gmail.com>
2025-06-24 11:49:41 +0700

    Chat Detail Component Design

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -4,14 +4,12 @@ const Detail = () => {
   return (
     <div className="detail">
       <div className="user">
-        <img src="./avatar.png" alt="" />
-        <h2>Paulus Dachi</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis iste
-          laboriosam molestias, tenetur doloribus reprehenderit modi, cumque aut
-          temporibus corporis praesentium cupiditate non aliquid error est
-          maxime architecto earum placeat?
-        </p>
+        <img
+          src="https://ui-avatars.com/api/?name=C&background=111928&color=ACACAB"
+          alt=""
+        />
+        <h2>Carollous Dachi</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
       </div>
       <div className="info">
         <div className="option">
@@ -22,8 +20,66 @@ const Detail = () => {
         </div>
         <div className="option">
           <div className="title">
-            <span>Chat Settings</span>
-            <img src="./arrowUp.png" alt="" />
+            <span>Privacy & Help</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-waterfall-free-image.jpeg?w=600&quality=80"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-waterfall-free-image.jpeg?w=600&quality=80"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-waterfall-free-image.jpeg?w=600&quality=80"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-waterfall-free-image.jpeg?w=600&quality=80"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-waterfall-free-image.jpeg?w=600&quality=80"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" className="icon" alt="" />
+            </div>
           </div>
         </div>
         <div className="option">
@@ -32,12 +88,7 @@ const Detail = () => {
             <img src="./arrowUp.png" alt="" />
           </div>
         </div>
-        <div className="option">
-          <div className="title">
-            <span>Chat Settings</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
+        <button>Block User</button>
       </div>
     </div>
   );
